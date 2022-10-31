ff344aad0f4aa094ad664e92afb6142d57f404ca
Ольга Мелихова <melihova.o@yandex.ru>
2022-10-31 17:18:36 +0300

    to check

--- homework.py
+++ homework.py
@@ -1,11 +1,27 @@
-...
+import logging
+import os
+import time
+from http import HTTPStatus
+
+import requests
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,63 +36,88 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщение в чат."""
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logging.info('Сообщение отправлено успешно.')
+    except Exception:
+        logging.error('Не удалось отправить сообщение!')
 
 
 def get_api_answer(current_timestamp):
+    """Делает запрос к эндпоинту сервиса."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    try:
+        homework_status = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params)
+    except Exception as error:
+        logging.error(f'Эндпоинт недоступен!: {error}')
+    if homework_status.status_code != HTTPStatus.OK:
+        status_code = homework_status.status_code
+        raise Exception(f"Ошибка {status_code}")
+    return homework_status.json()
 
 
 def check_response(response):
-
-    ...
+    """Проверяет ответ API на корректность."""
+    if not isinstance(response, dict):
+        raise TypeError('Ответ API не является словарем.')
+    homeworks = response.get('homeworks')
+    if 'homeworks' not in response:
+        raise KeyError('Отсутствует ключ homeworks.')
+    if 'current_date' not in response:
+        raise KeyError('Отсутствует ключ current_date.')
+    if not isinstance(homeworks, list):
+        raise TypeError('Ответ API не является списком.')
+    return homeworks
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
+    """Извлекает из информации о конкретной
+     домашней работе статус этой работы."""
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_name is None:
+        message = 'Нет домашней работы'
+        raise KeyError(message)
+    if homework_status is None:
+        message = 'Нет новых статусов'
+        raise KeyError(message)
+    verdict = HOMEWORK_STATUSES[homework_status]
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
+    if check_tokens() == 'False':
+        logging.error('Не доступна переменная окружения!')
+        raise Exception
+    logging.info('Все переменные окружения доступны.')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
-    ...
-
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homeworks = check_response(response)
+            if homeworks:
+                homework_status = parse_status(homeworks[0])
+                send_message(bot, homework_status)
+            current_timestamp = response.get('current_date')
             time.sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error('Сбой в работе программы!')
+            send_message(bot, message)
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
